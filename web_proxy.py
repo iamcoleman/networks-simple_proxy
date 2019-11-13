50764504d82cdaaed1ac90cf7d5e9b133e566c91
Coleman <coleman.lyski@gmail.com>
2019-11-13 15:01:16 -0500

    final tweaks

--- web_proxy.py
+++ web_proxy.py
@@ -4,11 +4,12 @@ import threading
 import socket
 
 """
-Variables for the Proxy
+Global Variables for the Proxy
 """
-QUEUE = 25                  # max number of pending connections
+QUEUE = 40                  # max number of pending connections
 MAX_DATA_RECEIVE = 500000   # max number of bytes to receive at once
 PORT_NUMBER = 17771         # port number of proxy
+HOST = 'localhost'          # host of proxy
 DEBUG = True                # True for debug messages
 
 
@@ -17,7 +18,7 @@ Main Program
 """
 def main():
     port = PORT_NUMBER  # port number set above
-    host = ''           # blank for `localhost`
+    host = HOST         # host set above
 
     print('Proxy Server running on {}:{}'.format('localhost', port))
 
@@ -29,14 +30,14 @@ def main():
         proxSocket.listen(QUEUE)
     except:
         print('Could not open socket')
-        return
+        sys.exit(1)
 
     # listen for connections from client
     while True:
         # establish the connection
         clientSocket, clientAddress = proxSocket.accept()
 
-        # create a thread for every request
+        # create a new thread for every request
         connectionThread = threading.Thread(target=proxy_thread, args=(clientSocket, clientAddress))
         connectionThread.setDaemon(True)
         connectionThread.start()
@@ -48,67 +49,74 @@ Proxy Thread
 def proxy_thread(clientSocket, clientAddress):
     # receive the data
     clientRequest = clientSocket.recv(MAX_DATA_RECEIVE)
-    print(clientRequest)
+
+    if DEBUG:
+        print('\nClient Request:')
+        print(clientRequest)
 
     # parse the request
     first_line = clientRequest.split(b'\n')[0]
-    url = clientRequest.split(b' ')[1]
+    url = b''
+    if len(clientRequest) > 1:
+        url = clientRequest.split(b' ')[1]
 
     if DEBUG:
-        print('First Line: {}'.format(first_line))
-        print('URL: {}'.format(url))
+        print('\nRequest First Line: {}'.format(first_line))
+        print('Request URL: {}'.format(url))
 
     # find the webserver and the port
     http_pos = url.find(b'://')
-    temp = url if http_pos < 0 else url[http_pos+3:]
-    port_pos = temp.find(b':')
-    webserver_pos = temp.find(b'/')
-    if webserver_pos < 0:
-        webserver_pos = len(temp)
+    temp_web_server = url if http_pos < 0 else url[http_pos+3:]
+    port_pos = temp_web_server.find(b':')
+    web_server_pos = temp_web_server.find(b'/')
+    if web_server_pos < 0:
+        web_server_pos = len(temp_web_server)
 
-    webserver = ''
-    port = -1
-    if port_pos < 0 or webserver_pos < port_pos:
+    web_server = ''
+    web_server_port = -1
+    if port_pos < 0 or web_server_pos < port_pos:
         # default port
-        port = 80
-        webserver = temp[:webserver_pos]
+        web_server_port = 80
+        web_server = temp_web_server[:web_server_pos]
     else:
         # specific port
-        port = int(temp[port_pos+1:][:webserver_pos-port_pos-1])
-        webserver = temp[:port_pos]
+        web_server_port = int(temp_web_server[port_pos + 1:][:web_server_pos - port_pos - 1])
+        web_server = temp_web_server[:port_pos]
 
     if DEBUG:
-        print('Connect to: {} {}'.format(webserver, port))
+        print('\nConnect to: {} {}'.format(web_server, web_server_port))
 
     # create a socket to connect to the web server
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((webserver, port))
+        s.connect((web_server, web_server_port))
         s.send(clientRequest)
 
         while True:
-            # receive data from web server
+            # receive data from web server so send back to client
             try:
                 data = s.recv(MAX_DATA_RECEIVE)
             except socket.timeout as e:
                 err = e.args[0]
+                # continue if timeout error
                 if err == 'timed out':
-                    print('recv timed out, retry later')
+                    print('\nError: recv timed out, retry later')
                     continue
+                # kill proxy if another error
                 else:
-                    print('Error: {}'.format(e))
+                    print('\nError: {}'.format(e))
                     sys.exit(1)
             except socket.error as e:
-                print('Error: {}'.format(e))
+                print('\nError: {}'.format(e))
                 sys.exit(1)
 
             if DEBUG:
-                print('Data: {}'.format(data))
+                print('\nData Received: {}'.format(data))
 
             if len(data) > 0:
                 # send to browser
                 if DEBUG:
-                    print('Sending to browser: {}'.format(data))
+                    print('Sending to Browser: {}'.format(data))
                 clientSocket.send(data)
             else:
                 # connection is closed
@@ -118,7 +126,7 @@ def proxy_thread(clientSocket, clientAddress):
         s.close()
         clientSocket.close()
     except:
-        print('Unable to create socket')
+        print('Error: Unable to create socket')
         if s:
             s.close()
         if clientSocket:
